927e4af6fe7228b81e07a8e9a3d17f27a47578d3
Bengt Berge <postbb@gmail.com>
2010-11-20 13:47:36 +0100

    First working POC

---
+++ SampleSite/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WatiN_MVC.SampleSite.Models
+{
+    public class Profile
+    {
+        public string Name {get; set;}
+        public string Email {get; set;}
+        public string Website {get; set;}
+        public string Company {get; set;}
+        public string Location {get; set;}
+    }
+}
--- WatiN-MVC.Tests/NUnitSampleTests.cs
+++ WatiN-MVC.Tests/NUnitSampleTests.cs
@@ -1,8 +1,32 @@
 using NUnit.Framework;
+using WatiN.Core;
+using System.Web.Mvc;
+using WatiN_MVC.SampleSite.Models;
+using System.Linq.Expressions;
+using System;
 
 namespace WatiN_MVC.Tests {
-    [TestFixture]
-    public class NUnitSampleTests {
+
+    public class WatinMvcBase<TModel> where TModel : class
+    {
+        protected void TypeInTextBox<TProperty>(IE browser, Expression<Func<TModel, TProperty>> expression, string value)
+        {
+            string name = ExpressionHelper.GetExpressionText(expression);
+            browser.TextField(Find.ByName(name)).TypeText(value);
+        }
+
+        protected string TextBoxFor<TProperty>(Expression<Func<TModel, TProperty>> expression) 
+        {
+            return ExpressionHelper.GetExpressionText(expression);
+        }
+
+    }
+
+
+    public class NUnitSampleTests : WatinMvcBase<Profile> {
+
+        string url = "http://localhost/WatiN-MVC/Profile/Edit/1";
+
         [Test]
         public void SomePassingTest() {
             Assert.AreEqual(5, 5);
@@ -12,5 +36,21 @@ namespace WatiN_MVC.Tests {
         public void SomeFailingTest() {
             Assert.Greater(5, 7);
         }
+
+        [Test]
+        public void SearchForWatiNOnGoogle()
+        {
+            using (var browser = new IE(url))
+            {
+                TypeInTextBox(browser, model => model.Email, "Dette var jo morro");
+
+                Assert.IsTrue(browser.ContainsText("Email"));
+            }
+
+        }
+
+
+
+
     }
 }
